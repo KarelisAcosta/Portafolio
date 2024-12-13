f76d69a9fbbc2baa0c1c48eaf203dbf149392d79
Kare <acostakare16@gmail.com>
2024-12-13 14:36:21 -0500

    deploy v2

--- src/router/index.js
+++ src/router/index.js
@@ -6,32 +6,32 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: () => import("../views/HomeView.vue"),
+      component: () => import("./../views/HomeView.vue"),
     },
     {
       path: "/about",
       name: "about",
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("./../views/AboutView.vue"),
     },
     {
       path: "/skills",
       name: "skills",
-      component: () => import("../views/SkillsView.vue"),
+      component: () => import("./../views/SkillsView.vue"),
     },
     {
       path: "/projects/:category",
       name: "projects",
-      component: () => import("../views/ProjectsView.vue"),
+      component: () => import("./../views/ProjectsView.vue"),
     },
     {
       path: "/project",
       name: "project",
-      component: () => import("../views/ProjectView.vue"),
+      component: () => import("./../views/ProjectView.vue"),
     },
     {
       path: "/project/:name",
       name: "project",
-      component: () => import("../views/ProjectView.vue"),
+      component: () => import("./../views/ProjectView.vue"),
     },
   ],
 });
