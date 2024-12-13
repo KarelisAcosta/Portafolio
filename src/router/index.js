24d45af2954a545c069c307d8b1ebc01b6d1cbea
Kare <acostakare16@gmail.com>
2024-12-13 14:40:59 -0500

    deploy v3

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,7 @@ const router = createRouter({
       name: "home",
       component: () => import("./../views/HomeView.vue"),
     },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("./../views/AboutView.vue"),
-    },
+
     {
       path: "/skills",
       name: "skills",
